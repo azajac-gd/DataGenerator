740de8a47efaba50a3b756d27689b1939a5f1749
azajac-gd <azajac@griddynamics.com>
2025-05-10 09:06:57 +0200

    Changing client to OpenAI()

--- Task1/services/openai_service.py
+++ Task1/services/openai_service.py
@@ -1,13 +1,13 @@
-import openai
+from openai import OpenAI
 
 class OpenAIService:
     def __init__(self, api_key: str):
-        openai.api_key = api_key
+        self.client = OpenAI(api_key=api_key)
 
     def generate_code(self, prompt: str, model="gpt-4.1-mini") -> str:
-        response = openai.ChatCompletion.create(
+        response = self.client.chat.completions.create(
             model=model,
             messages=[{"role": "user", "content": prompt}],
             temperature=0.7
         )
-        return response.choices[0].message["content"]
+        return response.choices[0].message.content
