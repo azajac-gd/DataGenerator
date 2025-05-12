b366ea8d3b3ca7ed66381124ab4b5400c64c15ab
azajac-gd <azajac@griddynamics.com>
2025-05-12 11:48:00 +0200

    cleaning files structure

--- Task1/utils/execution.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def exec_generated_code(code: str) -> pd.DataFrame:
-    local_vars = {}
-    exec(code, {}, local_vars)
-    for var in local_vars.values():
-        if isinstance(var, pd.DataFrame):
-            return var
-    raise ValueError("No DataFrame found in generated code.")
